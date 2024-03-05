927139e75a53b4b34812b0c8ef24e35bad758614
NelsonKinyanjui <nelsonkinyanjui246@gmail.com>
2024-03-05 09:30:41 +0300

    add alternative for onclick attributes on js

--- assets/events.js
+++ assets/events.js
@@ -1,3 +1,9 @@
-const btn = document.querySelector('#btn');
+const btn = document.querySelector('.btn');
 
  btn.onclick = () => alert('Hello world!');
+
+ const btnTwo = document.querySelector('.btnTwo');
+
+ btnTwo.addEventListener('click', () => {
+    alert("I said don't click me!");
+ });
